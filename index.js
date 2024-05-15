ccd2bcd52e69c2df782e0654a99c3d0285dddcb0
Flutterneu <rsmx141@gmail.com>
2024-05-15 14:07:56 +0530

    updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 const consumerKey = "ck_086b844aceb48a953bd8b8c9ab602bae23223127";
 const consumerSecret = "cs_a975ef002226e7b23b85adeeb18cabf1c7d9df9a";
-const apiUrl = `https://srtgroceries.com/wp-json/wc/v3/products?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}&per_page=100`;
+const apiUrl = `https://srtgroceries.com/wp-json/wc/v3/products?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`;
 
 let products = [];
 
