6ce4c41a8b67e2c9a0c52a9d040288699ee2fb06
Flutterneu <rsmx141@gmail.com>
2024-05-16 10:40:15 +0530

    index updated

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const axios = require("axios");
-const NodeCache = require("node-cache");
 
 const app = express();
 const port = process.env.PORT || 3000; // Use the PORT environment variable if available
 
-const consumerKey =
-  process.env.CONSUMER_KEY || "ck_086b844aceb48a953bd8b8c9ab602bae23223127";
-const consumerSecret =
-  process.env.CONSUMER_SECRET || "cs_a975ef002226e7b23b85adeeb18cabf1c7d9df9a";
+const consumerKey = "ck_086b844aceb48a953bd8b8c9ab602bae23223127";
+const consumerSecret = "cs_a975ef002226e7b23b85adeeb18cabf1c7d9df9a";
 const apiUrl = `https://srtgroceries.com/wp-json/wc/v3/products?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`;
 
-const productCache = new NodeCache({ stdTTL: 3600, checkperiod: 120 }); // Cache for 1 hour
+let products = [];
 
 const fetchProducts = async () => {
   let allProducts = [];
@@ -27,12 +24,11 @@ const fetchProducts = async () => {
     } catch (error) {
       console.error("Error fetching products:", error);
       fetchedProducts = [];
-      break; // Exit the loop on error
     }
   } while (fetchedProducts.length > 0);
 
-  productCache.set("products", allProducts);
-  console.log("Fetched products:", allProducts.length); // Log the number of fetched products
+  products = allProducts;
+  console.log("Fetched products:", products.length); // Log the number of fetched products
 };
 
 // Fetch products initially and then every hour
@@ -47,7 +43,6 @@ app.get("/autocomplete", (req, res) => {
     return res.status(400).json({ error: "Query parameter q is required" });
   }
 
-  const products = productCache.get("products") || [];
   const lowerCaseQuery = query.toLowerCase();
   const results = products.filter((product) =>
     product.name.toLowerCase().includes(lowerCaseQuery)
